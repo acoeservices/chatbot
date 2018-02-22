1901f8ac4cf31ebd1a957719d15787a7e7a65371
venoleti <venoleti@cox.com>
2018-02-22 17:39:33 -0500

    initilising the chatbot

---
+++ chatbot.js
@@ -0,0 +1,32 @@
+'use strict'
+const ecpress = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+
+const app = express()
+app.set('port',(process.env.PORT || 5000)
+
+//Allows us to process the data
+
+app.use(bodyParser.urlenocded({extended: false})))
+
+app.use(bodyParser.json())
+
+//Routes
+
+app.get('/',function(req,res){
+    res.send("Sai i am chat bot");
+})
+
+//Facebok
+
+app.get('/webhook',function(req,res){
+    if(req.query['hub.verify_token']==="blondiebutes"){
+        req.send(req.query['hub.challenge'])
+    }
+    res.send("Wrong Token")
+})
+
+app.listen(app.get('port'),function(){
+    console.log("running : port");
+});
